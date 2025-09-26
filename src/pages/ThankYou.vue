ff170b9f0a9b567144461826707fe88b78087e71
Wins0309 <winstonl3900@gmail.com>
2025-09-26 23:39:30 +1000

    center thank you page

--- src/pages/ThankYou.vue
+++ src/pages/ThankYou.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="c-thank-you bg-[#6C5545] h-[100vh] min-h-[100vh] max-h-[100vh]">
     <div class="w-full h-full flex items-center justify-center text-white">
-      <h2>Thank you for contacting us. <br /> We will get in touch shortly</h2>
+      <h2 class="text-center">Thank you for contacting us. <br /> We will get in touch shortly</h2>
     </div>
   </section>
 </template>
